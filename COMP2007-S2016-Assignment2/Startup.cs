e9eca78da752abdcfd2f7ed3790bd69c0a19aa0c
AnthonyGobin <anthony_g0404@hotmail.com>
2016-07-28 12:15:31 -0400

    Add project files.

---
+++ COMP2007-S2016-Assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Assignment2.Startup))]
+namespace COMP2007_S2016_Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
