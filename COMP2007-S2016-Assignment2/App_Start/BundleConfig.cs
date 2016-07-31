26953958d9f00f1a1088ca554999b5eae2f9ea5d
AnthonyGobin <anthony_g0404@hotmail.com>
2016-07-31 19:03:46 -0400

    Images for items, and contact logos

--- COMP2007-S2016-Assignment2/App_Start/BundleConfig.cs
+++ COMP2007-S2016-Assignment2/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace COMP2007_S2016_Assignment2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css"));
         }
     }
 }
